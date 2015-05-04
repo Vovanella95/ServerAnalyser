e3a57f5ecf54cf127032bc73b2458126245eac74
Uladzimir Paliukhovich <Uladzimir_Paliukhovich@epam.com>
2015-05-04 17:28:55 +0300

    created analyse methods

--- ServerAnalyser/Program.cs
+++ ServerAnalyser/Program.cs
@@ -15,19 +15,8 @@ namespace ServerAnalyser
         {
             const string path = @"C:\Users\Uladzimir_Paliukhovi\Desktop\PackedHeaders2";
             var data = Analyser.GetAllContents(path);
-           /* var ccc = aaa.Where((w => w.Contains("Jetty")));
-            int i = 0;
-            foreach (var item in ccc)
-            {
-                Console.WriteLine(item);
-                i++;
-                if (i > 100)
-                {
-                    break;
-                }
-            }*/
 
-            Console.WriteLine(Analyser.Analyse(data, "Microsoft-IIS/7.5"));
+            Analyser.PrintInfo(Analyser.Analyse(data, "Microsoft-IIS"));
             //Class1.Analyse(aaa, "Microsoft-IIS");
             
             Console.ReadLine();
--- ZipExtractor/Analyser.cs
+++ ZipExtractor/Analyser.cs
@@ -41,32 +41,22 @@ namespace ZipExtractor
             return null;
         }
 
-        public static string Analyse(IEnumerable<string> e, string serverName)
+        public static ServerInfo Analyse(IEnumerable<string> e, string serverName)
         {
             var name = GetName(serverName);
-            var version = GetVersion(serverName);
-            var count = 0;
-            var countGlobal = 0;
-            var countVersion = 0;
-            var versions = new List<string>();
+            int globalCount = 0;
+            ServerInfo si = new ServerInfo(GetName(serverName));
 
             foreach (var item in e)
             {
                 if (GetName(item) == name)
                 {
-                    countGlobal++;
-                    if (GetVersion(item) == version)
-                    {
-                        countVersion++;
-                    }
-                    if (GetVersion(item) != "" && !versions.Contains(GetVersion(item)))
-                    {
-                        versions.Add(GetVersion(item));
-                    }
+                    si.Add(GetVersion(item));
                 }
-                count++;
+                globalCount++;
             }
-            throw new NotImplementedException();
+            si.GlobalCount = globalCount;
+            return si;
         }
 
         public static string GetVersion(string serverName)
@@ -79,12 +69,61 @@ namespace ZipExtractor
         {
             return serverName.Replace("Server: ","").Replace("\r","").Split('\\', '/', '(',' ').First();
         }
+
+        public static void PrintInfo(ServerInfo server)
+        {
+           Console.WriteLine("Server: "+server.Name+" ("+(server.ThisCount*100.0/server.GlobalCount)+"% Of All Servers)");
+
+            double percent = 0;
+            foreach (var version in server.Versions)
+            {
+                var localPercent = version.Count*100.0/server.ThisCount;
+                if (localPercent>5)
+                {
+                    Console.WriteLine((version.Name!=""? version.Name: "No version")+"  -  "+localPercent+"%");
+                    percent += localPercent;
+                }
+            }
+            Console.WriteLine("Other:  -  "+ (100 - percent)+"%");
+
+        }
     }
 
     public class VersionInfo
     {
-        public string VersionName;
-        public int VersionCount;
+        public string Name;
+        public int Count;
+    }
+
+    public class ServerInfo
+    {
+        public List<VersionInfo> Versions;
+        public string Name;
+        public int GlobalCount;
+        public int ThisCount;
+        public ServerInfo(string name)
+        {
+            Name = name;
+            Versions = new List<VersionInfo>();
+            GlobalCount = 0;
+            ThisCount = 0;
+        }
+        public void Add(string version)
+        {
+            if (Versions.Count(w => w.Name == version)==0)
+            {
+                Versions.Add(new VersionInfo()
+                {
+                    Name = version,
+                    Count = 1
+                });
+            }
+            else
+            {
+                Versions.First(w=>w.Name==version).Count+=1;
+            }
+            ThisCount += 1;
+        }
     }
 
 
